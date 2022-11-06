20492ceebdcb5765c04160342184a7aa2dfc8103
Melissa Alessi <Joe44alessi@gmail.com>
2022-11-06 11:22:44 -0500

    1

--- web/src/components/AdvancedBanner.tsx
+++ web/src/components/AdvancedBanner.tsx
@@ -31,76 +31,7 @@ const AdvancedBannerTop = ({ content }: Props) => {
     expanded: false,
     children: (
       <div style={{ margin: "100px" }} className="inset center">
-        <MDBRow className="row-cols-1 row-cols-md-3 g-4">
-          <MDBCol>
-            <MDBCard>
-              <MDBCardImage
-                src="https://mdbootstrap.com/img/new/standard/city/041.webp"
-                alt="..."
-                position="top"
-              />
-              <MDBCardBody>
-                <MDBCardTitle>Card title</MDBCardTitle>
-                <MDBCardText>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </MDBCardText>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-          <MDBCol>
-            <MDBCard>
-              <MDBCardImage
-                src="https://mdbootstrap.com/img/new/standard/city/042.webp"
-                alt="..."
-                position="top"
-              />
-              <MDBCardBody>
-                <MDBCardTitle>Card title</MDBCardTitle>
-                <MDBCardText>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </MDBCardText>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-          <MDBCol>
-            <MDBCard>
-              <MDBCardImage
-                src="https://mdbootstrap.com/img/new/standard/city/043.webp"
-                alt="..."
-                position="top"
-              />
-              <MDBCardBody>
-                <MDBCardTitle>Card title</MDBCardTitle>
-                <MDBCardText>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </MDBCardText>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-          <MDBCol>
-            <MDBCard>
-              <MDBCardImage
-                src="https://mdbootstrap.com/img/new/standard/city/044.webp"
-                alt="..."
-                position="top"
-              />
-              <MDBCardBody>
-                <MDBCardTitle>Card title</MDBCardTitle>
-                <MDBCardText>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </MDBCardText>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
+
       </div>
     ),
   };
